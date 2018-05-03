ab49ff022f3aebc8cb2fad584f2deb401ecd6e00
paiai <leesu@leesuui-MacBook-Pro.local>
2018-05-03 15:48:59 +0900

    node app test

---
+++ myApp/config/database.js
@@ -0,0 +1,33 @@
+module.exports = {
+  host     : 'localhost',
+  user     : 'root',
+  password : 'dltjsdn',
+  port     : '3306',
+  database : 'nodeapp'
+};
+
+{/* module.exports = (function () {
+    return {
+      local: { // localhost
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'dltjsdn',
+        database: ''
+      },
+      real: { // real server db info
+        host: '',
+        port: '',
+        user: '',
+        password: '',
+        database: ''
+      },
+      dev: { // dev server db info
+        host: '',
+        port: '',
+        user: '',
+        password: '',
+        database: ''
+      }
+    }
+  })(); */}
---
+++ myApp/routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var Thumbnail = require('thumbnail');
+var fs = require('fs');
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function(req,file, cb){
+    cb(null, './temp')
+  },
+  filename: function(req, file, cb){
+    cb(null, file.originalname)
+  }
+});
+
+var upload = multer({storage:storage});
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/thumbnail', function(req, res, next) {
+  res.render('thumbnail', { title: 'thumbnail' });
+});
+
+
+router.post('/upload', upload.single('uploadImg'), function(req,res){
+  console.log(req.file);
+  var filename = req.file.filename;
+  var thumbnail = new Thumbnail('./temp',  './thumbnails');
+  //ensureThumbnail(filename, imgWidth, imgHeight, callback)
+  thumbnail.ensureThumbnail(filename, null, 100, function(err, filename){
+      if(err) console.error(err);
+      console.log(filename);
+      res.redirect('/');
+  });
+}); 
+module.exports = router;
